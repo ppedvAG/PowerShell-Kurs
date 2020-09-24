e17782b65982e54cefff677295e1063063aeb177
Stefan Ober <StefanO@ppedv.de>
2020-09-24 16:33:26 +0200

    Beispiel für Parameterset hinzugefügt

---
+++ Skriptbeispiele/ParameterSetname.ps1
@@ -0,0 +1,11 @@
+﻿[cmdletBinding()]
+param(
+    [Parameter(Mandatory=$true, ParameterSetName="Set1")]
+    $Param1,
+    [Parameter(Mandatory=$true, ParameterSetName="Set2")]
+    $Param2,
+    [Parameter(Mandatory=$false, ParameterSetName="Set1")]
+    [Parameter(Mandatory=$true, ParameterSetName="Set2")]
+    $Param3
+
+)
