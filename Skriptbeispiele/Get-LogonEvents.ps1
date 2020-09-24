2884f23b3b878280bd0d41257de1fe3b90533e19
Stefan Ober <StefanO@ppedv.de>
2020-09-24 10:43:33 +0200

    Get-LogonEvents Skript mit Verbose und Debug ausgabe versehen

--- Skriptbeispiele/Get-LogonEvents.ps1
+++ Skriptbeispiele/Get-LogonEvents.ps1
@@ -1,8 +1,15 @@
-﻿Param(
+﻿[cmdletBinding()]
+Param(
 $Logname = "Security",
 $Computername = "localhost",
 [Parameter(Mandatory=$true)]
 [int]$EventId,
 $NewestEvents = 5
 )
+#Ausführliche Ausgabe wenn Skript mit -Verbose aufgerufen wird
+Write-Verbose -Message "Es wurden vom user folgende Werte übergeben: EventId = $EventId"
+
+#Debug "Haltepunkt" Wenn Skrupt mit -Debug aufgerufen wird
+Write-Debug -Message "Vor Eventlogabfrage"
+
 Get-EventLog -LogName $Logname -ComputerName $Computername | Where-Object -FilterScript {$_.EventID -eq $EventId} | Select-Object -First $NewestEvents
