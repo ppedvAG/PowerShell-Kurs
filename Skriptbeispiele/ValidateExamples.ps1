0d858a1a4cfaf6916838497e7c11f2dd564d936c
Stefan Ober <StefanO@ppedv.de>
2020-09-24 15:40:50 +0200

    Skript welches eine Function beeinhaltet hinzugefügt zur Veranschaulichung von Validates

---
+++ Skriptbeispiele/ValidateExamples.ps1
@@ -0,0 +1,16 @@
+﻿function Test-Validates
+{
+    [cmdletBinding()]
+    param(
+    [ValidatePattern("[a-z][0-9][A-Z][0-9]")]
+    [string]$Pattern,
+    [ValidateRange(1,5)]
+    [int]$Range,
+    [ValidateLength(2,5)]
+    [string]$Length,
+    [ValidateScript({Test-Path -IsValid -Path $_})]
+    [string]$Script,
+    [ValidateSet("Wert1","Wert2")]
+    [string]$Set
+    )
+}
