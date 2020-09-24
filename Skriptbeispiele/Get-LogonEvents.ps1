06dfdb92724562b261131caf5e18ea28d43e2fa6
Stefan Ober <StefanO@ppedv.de>
2020-09-24 10:12:57 +0200

    Kursbeispiele aus Einstiegsteil hinzugefügt

---
+++ Skriptbeispiele/Get-LogonEvents.ps1
@@ -0,0 +1,8 @@
+﻿Param(
+$Logname = "Security",
+$Computername = "localhost",
+[Parameter(Mandatory=$true)]
+[int]$EventId,
+$NewestEvents = 5
+)
+Get-EventLog -LogName $Logname -ComputerName $Computername | Where-Object -FilterScript {$_.EventID -eq $EventId} | Select-Object -First $NewestEvents
---
+++ Skriptbeispiele/InstallSkriptmitWait.ps1
@@ -0,0 +1,17 @@
+﻿
+#installiere etwas auf remote system 
+Restart-Computer -ComputerName Member1 -Force
+
+#Warte bis System offline
+do
+{
+    Start-Sleep -Seconds 10
+}while(Test-NetConnection -ComputerName Member1 -CommonTCPPort WINRM -InformationLevel Quiet)
+
+#Warte bis System wieder online
+do
+{
+    Write-Host -ForegroundColor DarkYellow "Warte"
+    Start-Sleep -Seconds 1
+    
+}until(Test-NetConnection -ComputerName Member1 -CommonTCPPort WINRM -InformationLevel Quiet)
