2afee0b6ae64372e7fd0dc3819fa2453b2523043
Stefan Ober <StefanO@ppedv.de>
2020-09-25 13:49:06 +0200

    DSC Beispiel eingefügt. Mit ausgelagertet Konfiguration Data

---
+++ DSC/WebServerConfig.psd1
@@ -0,0 +1,13 @@
+﻿@{
+    AllNodes = @(
+        @{
+            NodeName = "Member1"
+            WindowsFeature = @(
+            @{
+                Name = "Web-Server"
+                Ensure = "Present"
+            }
+            )
+            }
+    )
+}
---
+++ DSC/WebServerconfig.ps1
@@ -0,0 +1,23 @@
+﻿configuration WebServerconfiguration
+{
+    node $AllNodes.NodeName
+    {
+        LocalConfigurationManager
+        {
+            ConfigurationMode = "ApplyAndAutoCorrect" #Bei Abweichung Konfig wieder herstellen
+            ConfigurationModeFrequencyMins = 15 #Default 15 minuten, kleiner geht nicht, Wert in welchen Abständen geprüft wird
+        }
+
+        foreach($Feature in $Node.WindowsFeature)
+        {
+            WindowsFeature $Feature.Name
+            {
+                Ensure = $Feature.Ensure
+                Name   = $Feature.Name
+            }
+        }
+      
+    }
+}
+
+# ConfigurationName -configurationData <Pfad zur Datei "ConfigurationData.psd1">
